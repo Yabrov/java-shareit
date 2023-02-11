440ab24df8c924b30b9c8cecb33cecf8b83e6e62
yabro <yabrow@yandex.ru>
2023-02-11 20:01:08 +0500

    feat: created entities mappers

---
+++ src/main/java/ru/practicum/shareit/item/dto/mapper/ItemMapper.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.item.dto.mapper;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+@Component
+public class ItemMapper implements Converter<Item, ItemDto> {
+
+    @Override
+    public ItemDto convert(Item source) {
+        return new ItemDto(
+                source.getId(),
+                source.getName(),
+                source.getDescription(),
+                source.getAvailable(),
+                source.getRequest() != null ? source.getRequest().getId() : null
+        );
+    }
+}
