046aaa7557dc9c53337d4d82df45731ab87e9751
yabro <yabrow@yandex.ru>
2023-02-11 20:00:44 +0500

    feat: created DTOs

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,12 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class ItemDto {
+    private Integer id;
+    private String name;
+    private String description;
+    private Boolean available;
+    private Integer requestId;
 }
---
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -0,0 +1,10 @@
+package ru.practicum.shareit.user.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class UserDto {
+    private Integer id;
+    private String name;
+    private String email;
+}
