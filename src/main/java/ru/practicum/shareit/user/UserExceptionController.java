37dc4d66281a62fe6ad45e691365ec1fc51b41fb
TOP_GUN <yabrow@yandex.ru>
2023-02-18 19:56:46 +0500

    fix: updated exception controller

--- src/main/java/ru/practicum/shareit/user/UserExceptionController.java
+++ src/main/java/ru/practicum/shareit/user/UserExceptionController.java
@@ -2,15 +2,15 @@ package ru.practicum.shareit.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.user.exception.EmailUniqueViolationException;
 import ru.practicum.shareit.user.exception.NullEmailException;
 import ru.practicum.shareit.user.exception.UserNotFoundException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class UserExceptionController {
 
     @ResponseBody
