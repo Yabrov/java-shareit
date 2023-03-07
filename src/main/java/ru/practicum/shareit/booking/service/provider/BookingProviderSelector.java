42d3d35e603bd811e8f14341049b51047b605251
TOP_GUN <yabrow@yandex.ru>
2023-03-07 08:59:04 +0500

    fix: booking provider map externalizd into spring config

--- src/main/java/ru/practicum/shareit/booking/service/provider/BookingProviderSelector.java
+++ src/main/java/ru/practicum/shareit/booking/service/provider/BookingProviderSelector.java
@@ -5,9 +5,7 @@ import org.springframework.stereotype.Component;
 import ru.practicum.shareit.booking.Booking;
 import ru.practicum.shareit.booking.BookingState;
 
-import javax.annotation.PostConstruct;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,32 +13,15 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class BookingProviderSelector implements BookingProvider {
 
-    private final BookingProvider pastBookingProvider;
-    private final BookingProvider futureBookingProvider;
-    private final BookingProvider currentBookingProvider;
-    private final BookingProvider waitingBookingProvider;
-    private final BookingProvider rejectedBookingProvider;
-    private final BookingProvider defaultBookingProvider;
-
-    private final Map<BookingState, BookingProvider> providers = new HashMap<>();
-
-    @PostConstruct
-    private void initMap() {
-        providers.put(BookingState.PAST, pastBookingProvider);
-        providers.put(BookingState.FUTURE, futureBookingProvider);
-        providers.put(BookingState.CURRENT, currentBookingProvider);
-        providers.put(BookingState.WAITING, waitingBookingProvider);
-        providers.put(BookingState.REJECTED, rejectedBookingProvider);
-        providers.put(BookingState.ALL, defaultBookingProvider);
-    }
+    private final Map<BookingState, BookingProvider> providersMap;
 
     @Override
     public Collection<Booking> getAllBookingsOfUser(Long userId, BookingState state) {
-        return providers.get(state).getAllBookingsOfUser(userId, state);
+        return providersMap.get(state).getAllBookingsOfUser(userId, state);
     }
 
     @Override
     public Collection<Booking> getAllBookingsForOwnerItems(List<Long> itemIds, BookingState state) {
-        return providers.get(state).getAllBookingsForOwnerItems(itemIds, state);
+        return providersMap.get(state).getAllBookingsForOwnerItems(itemIds, state);
     }
 }
