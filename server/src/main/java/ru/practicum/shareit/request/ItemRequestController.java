3efac35e80c037d732c5f077a1c50cbf35443903
TOP_GUN <yabrow@yandex.ru>
2023-03-31 22:00:48 +0500

    fix: removed unused import

--- server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.service.ItemRequestService;
 
-import javax.validation.Valid;
 import java.util.Collection;
 
 @RestController
